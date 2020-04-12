9d9917d753ebc3d3dec37f9e5e49d9f87974877d
Noah <sheltielover432@gmail.com>
2020-04-11 22:00:00 -0400

    Better Explosions

---
+++ Action Platform/Assets/Scripts/CreateSmoke.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateSmoke : MonoBehaviour
+{
+    public GameObject smoke;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(smoke, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
